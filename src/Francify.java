9b801dfb1a18896ac3dc505cf893a1a678cef557
Taylor Wrobel <taylor.wrobel@gmail.com>
2012-08-31 15:53:24 -0400

    TAYLOR hates git

---
+++ src/Francify.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class Francify extends PApplet{
+
+	public void setup(){
+	    size(200,200);
+	    background(0);
+	}
+	
+	public void draw(){
+	    stroke(255);
+	    if (mousePressed) {
+	      line(mouseX,mouseY,pmouseX,pmouseY);
+	    }
+	}
+	
+}
